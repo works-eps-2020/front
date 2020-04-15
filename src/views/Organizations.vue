c0bb742217f7f56c16c294540321e5ac6cd5ff43
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2020-04-15 14:54:57 +0200

    add table to see organizations

---
+++ src/views/Organizations.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      title="Organizations"
+      :data="data"
+      :columns="columns"
+      row-key="name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Organizations',
+  data() {
+    return {
+			columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: (row) => row.name,
+          format: (val) => `${val}`,
+          sortable: true,
+        },
+        {
+          name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true,
+        },
+        {
+          name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true,
+        },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        {
+          name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+        {
+          name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+        },
+      ],
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%',
+        },
+      ],
+    };
+  },
+};
+</script>
